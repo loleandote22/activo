6beba1687d697adb76173564e63496f51d5a6116
Alfonso Sebastián Simorte Ramos <117114417+loleandote22@users.noreply.github.com>
2024-11-30 20:13:11 +0100

    fin cliente

---
+++ consumidor/csharp/csharp/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Newtonsoft.Json;
+
+namespace csharp
+{
+    public partial class MainWindow : Window
+    {
+        private IConnection connection;
+        private ISession session;
+        private IMessageConsumer consumer;
+        private IDestination destination;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void InitializeActiveMQConsumer(string tipo, string nombre)
+        {
+            try
+            {
+                // Crear una conexión al broker ActiveMQ
+                IConnectionFactory factory = new ConnectionFactory("activemq:tcp://localhost:61616");
+                connection = factory.CreateConnection();
+                connection.Start();
+
+                // Crear una sesión
+                session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+                // Crear un consumidor para la cola especificada
+                if (tipo.ToLower() == "queue")
+                    destination = session.GetQueue(nombre);
+                else
+                    destination = session.GetTopic(nombre);
+                consumer = session.CreateConsumer(destination);
+                Desconectar.IsEnabled = true;
+                Conectar.IsEnabled = false;
+                // Configurar el evento de recepción de mensajes
+                consumer.Listener += new MessageListener(OnMessageReceived);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al inicializar el consumidor de ActiveMQ: {ex.Message}");
+            }
+        }
+        private List<Color> colores = new List<Color> { Colors.Red, Colors.Blue, Colors.Green, Colors.Yellow, Colors.Purple, Colors.Orange, Colors.Pink, Colors.Brown, Colors.Black, Colors.White };
+        private Random random = new Random();
+        private void OnMessageReceived(Apache.NMS.IMessage message)
+        {
+            if (message is ITextMessage textMessage)
+            {
+                string text = textMessage.Text;
+                // Procesar el mensaje recibido
+                dynamic objeto = JsonConvert.DeserializeObject(text);
+                Dispatcher.Invoke(() => {
+                    int posicion = random.Next(colores.Count);
+                    Color color = colores[posicion];
+                    string mensaje = $"El contenido del mensaje es: {objeto["message"]} (de {destination})";
+                    Tarjeta tarjeta = new Tarjeta(color, mensaje);
+                    Tabla.Children.Add(tarjeta);
+                });
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            // Limpiar recursos
+            DesconectarDeActiveMQ();
+        }
+
+        private void Conectar_Click(object sender, RoutedEventArgs e)
+        {
+            string tipo = TipoTxtbox.Text;
+            string nombre = NombreTxtbox.Text;
+            if (tipo == "" || nombre == "")
+            {
+                MessageBox.Show("Por favor, ingrese un tipo y un nombre de suscriptor");
+                return;
+            }
+            InitializeActiveMQConsumer(tipo, nombre);
+            
+        }
+
+        private void Desconectar_Click(object sender, RoutedEventArgs e)
+        {
+            DesconectarDeActiveMQ();
+            Conectar.IsEnabled = true;
+            Desconectar.IsEnabled = false;
+        }
+        private void DesconectarDeActiveMQ()
+        {
+            try
+            {
+                consumer?.Close();
+                session?.Close();
+                connection?.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al desconectar de ActiveMQ: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ consumidor/csharp/csharp/Tarjeta.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace csharp
+{
+    /// <summary>
+    /// Lógica de interacción para Tarjeta.xaml
+    /// </summary>
+    public partial class Tarjeta : UserControl
+    {
+        public Tarjeta(Color fondo, String mensaje)
+        {
+            InitializeComponent();
+            Contenedor.BorderBrush = new SolidColorBrush(fondo);
+            Contenedor.Background = new SolidColorBrush(fondo);
+            Texto.Content = mensaje;
+            Texto.Foreground = new SolidColorBrush(ContrastColor(fondo));
+        }
+
+        private Color ContrastColor(Color color)
+        {
+            // Método para calcular el color de contraste
+            int d = 0;
+
+            // Obtener el brillo del color
+            double luminance = (0.299 * color.R + 0.587 * color.G + 0.114 * color.B) / 255;
+
+            if (luminance > 0.5)
+                d = 0; // color oscuro
+            else
+                d = 255; // color claro
+
+            return Color.FromRgb((byte)d, (byte)d, (byte)d);
+        }
+    }
+}
